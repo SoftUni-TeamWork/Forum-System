3b15bfa808f159c24502f029b1a8e3a72269d058
darinnn <georgiev.darin@gmail.com>
2014-08-26 21:39:11 +0300

    add style
    add style.css at Forum-System\public

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,61 +2,34 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Forum system</title>
-    <style>
-        li {
-            border-radius: 10px;
-
-            display: inline-block;
-            width: 140px;
-            padding: 3px;
-            background-color: #616161;
-            text-align: center;
-        }
-
-        a {
-            text-decoration: none;
-            color: white;
-            width: 100%;
-            display: inline-block;
-        }
-
-        label {
-            width: 140px;
-            display: inline-block;
-        }
-
-        section div:first-of-type {
-            width: 500px;
-            text-align: left;
-
-        }
-        footer a{
-            color: blue;
-            text-decoration: underline;
-            
-        }
-        footer div:first-of-type{
-            width: 100px;
-        }
-        
-    </style>
+    <!--<link rel="stylesheet" type="text/css" href="style.css" />-->
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Team "TAMBA" Forum system</title>
 </head>
 <body>
-<header>
-    <ul>
-        <li><a href="/">Начало</a></li>
-        <li><a href="/user/register">Регистрация</a></li>
-        <li><a href="/user/login">Вход в системата</a></li>
-    </ul>
-</header>
-<section>
-    <div>
-        @yield('content')
-    </div>
-</section>
-<footer>
-    <div><a href="https://softuni.bg">Softuni</a></div>
-</footer>
+<div class="wrapper">
+    <header>
+        <div class="logo">
+            <img alt="Team TAMBA Forum system" src="tamba3.png">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/">Начало</a></li>
+                <li><a href="/user/register">Регистрация</a></li>
+                <li><a href="/user/login">Вход</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div>
+                @yield('content')
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div><a href="https://softuni.bg">Softuni</a></div>
+    </footer>
+</div>
 </body>
 </html>
